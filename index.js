b1b90c396639a465625a371b5afc39c84b13eeba
Zel9278 <countbaster@gmail.com>
2023-11-11 22:31:23 +0900

    add prettier

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const { Client, Server } = require("node-osc");
-const { io } = require("socket.io-client");
-const { exec } = require("child_process");
-const iconv = require("iconv-lite");
-const os = require("os");
-const readline = require("readline");
-const { existsSync } = require("fs");
+const { Client, Server } = require("node-osc")
+const { io } = require("socket.io-client")
+const { exec } = require("child_process")
+const iconv = require("iconv-lite")
+const os = require("os")
+const readline = require("readline")
+const { existsSync } = require("fs")
 
-const client = new Client("127.0.0.1", 9000);
-const server = new Server(9001, "127.0.0.1");
+const client = new Client("127.0.0.1", 9000)
+const server = new Server(9001, "127.0.0.1")
 
-const CHAT_TEXT = `No Text`;
-const HOSTNAME = os.hostname();
+const CHAT_TEXT = `No Text`
+const HOSTNAME = os.hostname()
 
-let enable_gpu = false;
+let enable_gpu = false
 
 let statusData = {
     cpu: 0,
@@ -21,78 +21,91 @@ let statusData = {
     disk: {
         idleTime: "0",
         read: "0 Bytes",
-        write: "0 Bytes"
-    }
-};
-
-if (existsSync("C:\\Windows\\System32\\nvidia-smi.exe")) {
-    enable_gpu = true;
-    statusData.gpu = 0;
-    statusData.vmem = 0;
-    statusData.vmemData = ["0 Bytes", "0 Bytes"];
+        write: "0 Bytes",
+    },
 }
 
-const sio = new io("wss://pcss.eov2.com");
+if (existsSync("C:\\Windows\\System32\\nvidia-smi.exe")) {
+    enable_gpu = true
+    statusData.gpu = 0
+    statusData.vmem = 0
+    statusData.vmemData = ["0 Bytes", "0 Bytes"]
+}
+
+const sio = new io("wss://pcss.eov2.com")
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
-});
+    output: process.stdout,
+})
 
-let inputText = CHAT_TEXT;
+let inputText = CHAT_TEXT
 
-getPerf();
+getPerf()
 
-setInterval(() => getPerf(), 2000);
+setInterval(() => getPerf(), 2000)
 
 server.on("listening", () => {
-    console.log("OSC Server is listening.");
-	rl.on("line", (line) => {
-        if (line === "") return inputText = CHAT_TEXT;
-		inputText = line;
-	})
+    console.log("OSC Server is listening.")
+    rl.on("line", (line) => {
+        if (line === "") return (inputText = CHAT_TEXT)
+        inputText = line
+    })
 })
 
 server.on("message", (msg) => {
     //console.log(`Message: ${msg}`);
-});
+})
 
-sio.on("status", msg => {
-    const status = Object.values(msg).find(pc => pc.hostname === HOSTNAME);
-    const cpu = getCPUPercent(status.cpu.cpus);
-    const ram = getPercent(status.ram.free, status.ram.total);
-    const ramData = [byteToData(status.ram.total - status.ram.free), byteToData(status.ram.total)];
+sio.on("status", (msg) => {
+    const status = Object.values(msg).find((pc) => pc.hostname === HOSTNAME)
+    const cpu = getCPUPercent(status.cpu.cpus)
+    const ram = getPercent(status.ram.free, status.ram.total)
+    const ramData = [
+        byteToData(status.ram.total - status.ram.free),
+        byteToData(status.ram.total),
+    ]
     const disk = statusData.disk
 
     const data = {
         cpu,
         ram,
         ramData,
-        disk
+        disk,
     }
 
     if (enable_gpu) {
-        const gpu = status.gpu.usage;
-        const vmem = getPercent(status.gpu.memory.free, status.gpu.memory.total);
-        const vmemData = [byteToData((status.gpu.memory.total - status.gpu.memory.free) * 1024 * 1024), byteToData(status.gpu.memory.total * 1024 * 1024)];
+        const gpu = status.gpu.usage
+        const vmem = getPercent(status.gpu.memory.free, status.gpu.memory.total)
+        const vmemData = [
+            byteToData(
+                (status.gpu.memory.total - status.gpu.memory.free) *
+                    1024 *
+                    1024,
+            ),
+            byteToData(status.gpu.memory.total * 1024 * 1024),
+        ]
 
-        data.gpu = gpu;
-        data.vmem = vmem;
-        data.vmemData = vmemData;
+        data.gpu = gpu
+        data.vmem = vmem
+        data.vmemData = vmemData
     }
 
-    statusData = data;
-});
+    statusData = data
+})
 
 setInterval(() => {
-	const text = `${"["}${inputText}${"]"}`;
-    const gpu = enable_gpu ? ` | GPU: ${statusData.gpu}% | VMEM: ${statusData.vmem}%(${statusData.vmemData[0]}/${statusData.vmemData[1]})` : "";
-    send(`CPU: ${statusData.cpu}% | RAM: ${statusData.ram}%(${statusData.ramData[0]} / ${statusData.ramData[1]})${gpu} | DiskIO: ${statusData.disk.idleTime}%(r: ${statusData.disk.read} / w: ${statusData.disk.write}) | ${text}`)
+    const text = `${"["}${inputText}${"]"}`
+    const gpu = enable_gpu
+        ? ` | GPU: ${statusData.gpu}% | VMEM: ${statusData.vmem}%(${statusData.vmemData[0]}/${statusData.vmemData[1]})`
+        : ""
+    send(
+        `CPU: ${statusData.cpu}% | RAM: ${statusData.ram}%(${statusData.ramData[0]} / ${statusData.ramData[1]})${gpu} | DiskIO: ${statusData.disk.idleTime}%(r: ${statusData.disk.read} / w: ${statusData.disk.write}) | ${text}`,
+    )
 }, 3000)
 
-
 function send(text) {
-    client.send("/chatbox/input", text, true);
+    client.send("/chatbox/input", text, true)
 }
 
 function getPercent(free, total) {
@@ -100,46 +113,48 @@ function getPercent(free, total) {
 }
 
 function getCPUPercent(result) {
-    return result.length === 0 ? 0 : Math.floor(result.map((a) => a.cpu).reduce((a, b) => a + b) / result.length)
+    return result.length === 0
+        ? 0
+        : Math.floor(
+              result.map((a) => a.cpu).reduce((a, b) => a + b) / result.length,
+          )
 }
 
 function byteToData(a, b = 2) {
     if (0 >= a) return "0 Bytes"
     const c = 0 > b ? 0 : b,
         d = Math.floor(Math.log(a) / Math.log(1024))
-    const datas = [
-        "Bytes",
-        "KB",
-        "MB",
-        "GB",
-        "TB",
-        "PB",
-        "EB",
-        "ZB",
-        "YB",
-    ]
+    const datas = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
     return parseFloat((a / Math.pow(1024, d)).toFixed(c)) + " " + datas[d]
 }
 
 function iconvDecode(str = "") {
-    const encoding = "shift-jis";
-    const binaryEncoding = "binary";
+    const encoding = "shift-jis"
+    const binaryEncoding = "binary"
 
-    return iconv.decode(Buffer.from(str, binaryEncoding), encoding);
+    return iconv.decode(Buffer.from(str, binaryEncoding), encoding)
 }
 
 function getPerf() {
-    exec(`TypePerf "\\PhysicalDisk(_Total)\\% Idle Time"  "\\PhysicalDisk(_Total)\\Disk Read Bytes/sec"  "\\PhysicalDisk(_Total)\\Disk Write Bytes/sec" -s ${HOSTNAME} -SC 1`, { encoding: "binary" }, (error, stdout, stderr) => {
-        if (stderr && error) return
-        const res = iconvDecode(stdout);
-        const parse = res.split('\r\n').filter(v => v).map(v => v.replace(/"/g, ""))[1].split(",");
+    exec(
+        `TypePerf "\\PhysicalDisk(_Total)\\% Idle Time"  "\\PhysicalDisk(_Total)\\Disk Read Bytes/sec"  "\\PhysicalDisk(_Total)\\Disk Write Bytes/sec" -s ${HOSTNAME} -SC 1`,
+        { encoding: "binary" },
+        (error, stdout, stderr) => {
+            if (stderr && error) return
+            const res = iconvDecode(stdout)
+            const parse = res
+                .split("\r\n")
+                .filter((v) => v)
+                .map((v) => v.replace(/"/g, ""))[1]
+                .split(",")
 
-        const data = {
-            idleTime: Math.floor(100 - parse[1]),
-            read: byteToData(parse[2]),
-            write: byteToData(parse[3])
-        };
+            const data = {
+                idleTime: Math.floor(100 - parse[1]),
+                read: byteToData(parse[2]),
+                write: byteToData(parse[3]),
+            }
 
-        statusData.disk = data;
-    });
+            statusData.disk = data
+        },
+    )
 }
